8cd54f9624c6fed896c82091dcbf58fbfcf43673
Bela Hamid <belahamid@gmail.com>
2022-09-15 13:35:05 +0100

    Add exercises

---
+++ domevents/script.js
@@ -0,0 +1,40 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function toggleDone() {
+  var liElement = document.getElementByTagName(li);
+  console.log("What?", liElement);
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+li.addEventListener("click", toggleDone);
+
+// If you click on the list item, it toggles the .done  class on and off.
