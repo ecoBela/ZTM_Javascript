d6aab0801a0dbcb710f4127b40fef7807d783d3a
Bela Hamid <belahamid@gmail.com>
2022-09-27 18:41:18 +0100

    Advanced objects and arrays

--- advanced_exercise_6/exercise6.js
+++ advanced_exercise_6/exercise6.js
@@ -1,15 +1,36 @@
 //Evaluate these:
 //#1
 [2] === [2] //false
-{} === {} 
+{} === {} //false
 
 //#2 what is the value of property a for each object.
-const object1 = { a: 5 }; 
-const object2 = object1; 
-const object3 = object2; 
-const object4 = { a: 5}; 
-object1.a = 4;
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; //5
+object1.a = 4; //4
 
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+    constructor(name, type, colour){
+        this.name=name
+        this.type=type
+        this.colour=colour
+    }
+}
+
+class Mammal extends Animal{
+    constructor(name, type, colour){
+        super(name, type, colour)
+    }
+    sound(){
+       console.log(`My name is ${this.name} and I am a ${this.colour} ${ this.type}`) 
+
+    }
+}
+
+const cow = new Mammal("Mary", "Cow", "Brown")
+console.log(cow.sound)
